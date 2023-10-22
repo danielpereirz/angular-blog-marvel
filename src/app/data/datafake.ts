2d9c76632eba23ae6efa409ee6b4d08e0cc0f737
daniel <danielpereirapts@gmail.com>
2023-10-22 15:49:38 -0300

    new

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Beetlejuice 2 terá retorno de look clássico do protagonista, diz figurinista",
+    "description": "'Beetlejuice sem o seu terno listrado não é nada! É como fazer Alice no País das Maravilhas e não a colocar em um vestido azul. A questão é por quanto tempo o personagem usa aquele look que é quintessencial dele', provocou",
+    "photoCover":"https://cdn.ome.lt/KcPDDGg6sjFanLb88cc5qQt-mKA=/570x0/smart/uploads/conteudo/fotos/juice_sGS10PS.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Digimon Adventure 02: O Início ganha trailer e pôster",
+    "description": "Filme chega ao Brasil em 30 de novembro",
+    "photoCover":"https://cdn.ome.lt/t8-fHvzCaSn9awyFdlpr9HALi8U=/1200x630/smart/extras/conteudos/digimon-02-o-inicio-trailer-poster-capa.jpg"
+  }
+]
